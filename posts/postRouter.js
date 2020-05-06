d15fe5db56c119b856d64ea6239b8f38927db084
Manuel De La Mora <Manuel@defaults-mbp.attlocal.net>
2020-05-06 15:57:24 -0700

    Used middleware functions and build API to perform crud operations with endpoints to retrive data between posts and user.

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,21 +1,80 @@
 const express = require('express');
 
+const Posts = require("./postDb");
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  Posts.get()
+  .then(posts => {
+    res.status(200).json(posts);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({ message: 'Error retrieving posts.' });
+  });
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  Posts.getById(req.params.id)
+  .then(post => {
+    if(post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: 'Post not found' });
+    };
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({ message: 'Error retrieving post.'});
+  })
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  Posts.getById(req.params.id)
+      .then(post => {
+        if(post){
+          Posts.remove(req.params.id)
+            .then(item => {
+              if(item > 0){
+                res.status(200)
+              } else {
+                express.status(404).json({ message: 'Post with specified ID was not found.'});
+              };
+            })
+            .catch(error => {
+              console.log(error);
+              res.status(500).json({ messsage: 'Error deleting post with specified ID.'})
+            })
+          res.json(post);
+        }else{
+          express.status(404).json({ message: 'Post with specified ID was not found.'});
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ messsage: 'Error deleting post with specified ID.'})
+      })
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  Posts.getById(req.params.id)
+    .then(post => {
+      if(post){
+        let updatedPost = { ...req.body, user_id: post.user_id }
+        Posts.update(req.params.id, updatedPost)
+          .then(count => {
+            if(count === 1){
+              res.status(200).json(updatedPost)
+            } else {
+              res.status(404).json({ message: 'Post with specified ID was not found.'})
+            }
+          })
+          .catch(error => {
+            res.status(500).json({ message: 'Error updating posts.'})
+          })
+      }
+    })
 });
 
 // custom middleware
--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const postRouter = require("./posts/postRouter");
+const userRounter = require("./users/userRouter");
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(express.json());
+
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use("/api/posts", postRouter);
+server.use("/api/users", userRounter);
+
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(req.url);
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url}`
+  );
+  next();
+}
+
+server.listen(8000, () => {
+  console.log("API is running")
+})
 
 module.exports = server;
